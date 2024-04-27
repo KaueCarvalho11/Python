2dcd8bdf6a1f35b2be810aab7b545bd854bf19e6
Kauê Carvalho <kauecarvalho2028@gmail.com>
2024-04-26 21:04:19 -0300

    Checar tipo primitivo

---
+++ Tipos primitivos/type.py
@@ -0,0 +1,15 @@
+a = input("Informe o valor de a: ")
+print(a)
+print(type(a))
+
+b = int(input("Informe o valor de b: "))
+print(b)
+print(type(b))
+
+c = float(input("Informe o valor de c: "))
+print(c)
+print(type(c))
+
+d = bool(input("Informe o valor de d: "))
+print(d)
+print(type(d))
