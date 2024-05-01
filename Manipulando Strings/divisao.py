7ccbbade4697e6ffd5dc675ca1ebd920115b4142
Kauê Carvalho <kauecarvalho2028@gmail.com>
2024-04-30 22:55:04 -0300

    Utilizando e fazendo testes com as funções split e join

---
+++ Manipulando Strings/divisao.py
@@ -0,0 +1,10 @@
+frase = "Programando em Python"
+#split = Divide a string em uma lista com todas as palavras da string
+print("-".join(frase))
+print(frase.split())
+frase2 = frase.split()
+print("-".join(frase2))
+print(frase2[2])
+print(frase2[1])
+print(frase2[0])
+print(frase2[2][3])
