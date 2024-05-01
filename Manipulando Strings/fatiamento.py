5bf5010a1684361bd0663e8dbef5d6de7ee9ad3d
Kauê Carvalho <kauecarvalho2028@gmail.com>
2024-04-30 22:55:54 -0300

    Acessando string de acordo com a posição de seus caracteres

---
+++ Manipulando Strings/fatiamento.py
@@ -0,0 +1,13 @@
+frase = "Programando em python"
+print(frase[9])
+print(frase[0:10])
+print(frase[0:11])
+print(frase[0:20])
+print(frase[0:21])
+print(frase[0:22])
+print(frase[0:21:2])
+print(frase[:12])
+print(frase[12:])
+print(frase[3::3])
+print(frase[1::2])
+print(frase[::2])
