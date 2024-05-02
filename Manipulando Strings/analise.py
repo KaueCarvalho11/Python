dcf681e74977282ee883e52eedde5849a4477bea
Kauê Carvalho <kauecarvalho2028@gmail.com>
2024-05-01 22:10:26 -0300

    Analisando string

--- Manipulando Strings/analise.py
+++ Manipulando Strings/analise.py
@@ -2,7 +2,7 @@ frase = "Programando em python"
 print(len(frase))
 print(frase.count('a'))
 print(frase.count('a', 0, 7))
-print(frase.find('mando'))
+print(frase.find('mando'))  
 print(frase.find('maracuja'))
 print('python' in frase)
 print('maracuja' in frase)
