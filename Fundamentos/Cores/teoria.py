952f60440cc18f8cb69c797d85e601aafdf7390c
Kauê Carvalho <kauecarvalho2028@gmail.com>
2024-05-03 16:01:35 -0300

    Trabalhando com cores em python

---
+++ Fundamentos/Cores/teoria.py
@@ -0,0 +1,35 @@
+# Padrão ANSI
+
+#                       style          Background     
+# Representar cor: \033[      ;      ;             m
+#                               Text
+
+
+# Style
+
+# 0 = Nada
+# 1 = Negrito
+# 4 = Sublinhado
+# 7 = Inverte cor do fundo com a cor da letra
+
+# Text
+
+# 30 = Branco
+# 31 = Vermelho
+# 32 = Verde
+# 33 = Amarelo
+# 34 = Azul
+# 35 = Majenta
+# 36 = Ciano
+# 37 = Cinza
+
+# Background
+
+# 40 = Branco
+# 41 = Vermelho
+# 42 = Verde
+# 43 = Amarelo
+# 44 = Azul
+# 45 = Majenta
+# 46 = Ciano
+# 47 = Cinza
