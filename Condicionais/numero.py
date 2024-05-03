c2a1adfa70d1ded947418b034cd0b0070c1376c4
Kauê Carvalho <kauecarvalho2028@gmail.com>
2024-05-03 14:33:44 -0300

    Verificando se um número é par ou ímpar

---
+++ Condicionais/numero.py
@@ -0,0 +1,7 @@
+numero = int(input("Informe um número inteiro: "))
+
+if numero % 2 == 0:
+    print("{} é um número par!".format(numero))
+
+else:
+    print("{} é um número ímpar!".format(numero))
