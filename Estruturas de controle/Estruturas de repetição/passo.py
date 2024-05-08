3c982ecd9c14c92bfa021078a3c53cab20d0f95b
Kauê Carvalho <kauecarvalho2028@gmail.com>
2024-05-08 01:44:51 -0300

    Prática

---
+++ Estruturas de controle/Estruturas de repetição/passo.py
@@ -0,0 +1,6 @@
+inicio = int(input("Informe o início: "))
+fim = int(input("Informe o fim: "))
+passo = int(input("Informe o passo: "))
+
+for c in range (inicio, fim, passo):
+    print(c)
---
+++ Estruturas de controle/Estruturas de repetição/teste.py
@@ -0,0 +1,12 @@
+for c in range (0, 3):
+    print("Hello World")
+print("FIM")
+
+for j in range (1, 11):
+    print("\n",j)
+
+for k in range (10, 0, -1):
+    print("\n",k)
+
+for l in range(1, 11, 2):
+    print("\n",l)
