4670304c331993ac72d5a71d03e42f6e188ced23
Kauê Carvalho <kauecarvalho2028@gmail.com>
2024-05-08 01:41:30 -0300

    Contagem regressiva utilizando estruturas de repetição

---
+++ Estruturas de controle/Estruturas de repetição/ano.py
@@ -0,0 +1,16 @@
+from datetime import date
+
+maioridade = 0
+menoridade = 0
+
+for c in range (1, 8):
+    ano = int(input("Informe um ano de nascimento: "))
+    idade = date.today().year - ano
+    
+    if idade >= 21:
+        maioridade +=1
+
+    else:
+        menoridade +=1
+
+print("{} pessoas atingiram a maioridade\n{} pessoas ainda são de menor".format(maioridade, menoridade))
