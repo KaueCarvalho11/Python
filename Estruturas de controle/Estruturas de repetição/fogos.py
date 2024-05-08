848b85f96267dc1fc15591f075ee99f5bcdcff2c
Kauê Carvalho <kauecarvalho2028@gmail.com>
2024-05-08 01:43:00 -0300

    Contagem regressiva utilizando estruturas de repetição

---
+++ Estruturas de controle/Estruturas de repetição/fogos.py
@@ -0,0 +1,9 @@
+from time import sleep
+
+print("Faltam 10 segundos para a virada do ano! contagem regressiva: ")
+
+for c in range (10, - 1, -1):
+    print(c)
+    sleep(1)
+
+print("Feliz ano novo! ")
